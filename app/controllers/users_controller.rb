0e23781b7219d2b26337a3cfa227e268f05478a3
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-15 15:49:53 +0900

    ユーザーの詳細ページと登録後のリダイレクトを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功した場合の処理
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
