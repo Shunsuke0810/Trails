c1f1df5b47d89cb30e8dd2296c058add0fce19b8
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-15 16:50:50 +0900

    未ログインユーザーはログインページに飛ばすようbefore_actionを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  protect_from_forgery with: :exception
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
+
 end
