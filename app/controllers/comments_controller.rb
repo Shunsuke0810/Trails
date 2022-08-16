270006eb5388098c562ed04518471a7588282286
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-16 17:30:31 +0900

    view,controllerを設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.build(comment_params)
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to post_path(@post) }
+        else
+          format.html { redirect_to blog_path(@post), notice: '投稿できませんでした' }
+        end
+      end
+    end
+  
+    private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
   end
 end
