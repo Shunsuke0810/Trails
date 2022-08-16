72024e637e12d8834db0541f95437e2db1932321
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-16 17:55:36 +0900

    コメント編集機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :edit, :update]
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
@@ -10,9 +12,35 @@ class CommentsController < ApplicationController
       end
     end
   end
-
+  
+  def edit
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+  
+  def update
+    @comment = @post.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit }
+        end
+      end
+  end
+  
   private
   def comment_params
     params.require(:comment).permit(:content)
   end
+  
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+  
 end
