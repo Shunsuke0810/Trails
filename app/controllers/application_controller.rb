aed0be82fe5e0b5a73a05a33add27cb04326bbd2
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-18 18:32:26 +0900

    admin:trueのユーザーのadmin画面にログイン可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :login_required
 
+  def check
+    if current_user&.admin?
+      redirect_to rails_admin_path
+    else
+      redirect_to posts_path
+    end
+  end
+
+
   private
 
   def login_required
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get "/admin", to: "application#check"
+  mount RailsAdmin::Engine => '/admins', as: 'rails_admin'
   resources :labels
   resources :posts do
     resources :comments
