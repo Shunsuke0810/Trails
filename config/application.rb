adce037d226c60a4528f789a9cb3260161477e9f
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-22 11:58:49 +0900

    datetimeの表記を修正

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Trails
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+  
     config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
