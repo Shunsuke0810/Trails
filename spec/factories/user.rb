beecf6a6a648d4ff9ad3fe96a8268183a25f462c
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-26 12:54:42 +0900

    管理者テスト、ブックマークテスト実装

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,6 +8,15 @@ FactoryBot.define do
     position { 'driver' }
     unit_id { 1 }
   end
+  factory :user_b, :class => 'user' do
+    name { 'B' }
+    email {'test_b@test.com'}
+    password {'aaaaaa'}
+    password_confirmation {'aaaaaa'}
+    admin {'false'}
+    position { 'driver' }
+    unit_id { 1 }
+  end
   factory :user_post, :class => 'user' do
     name { 'A' }
     email {'test@test.com'}
