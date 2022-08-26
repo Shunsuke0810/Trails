cef39fae132978f91832d97287de565878d8383b
SHUNSUKE <shunsuke0810@gmail.com>
2022-08-26 11:09:06 +0900

    adminテスト実装

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -7,6 +7,33 @@ FactoryBot.define do
     admin {'false'}
     position { 'driver' }
     unit_id { 1 }
+  end
+  factory :user_post, :class => 'user' do
+    name { 'A' }
+    email {'test@test.com'}
+    password {'aaaaaa'}
+    password_confirmation {'aaaaaa'}
+    admin {'false'}
+    position { 'driver' }
+    unit_id { 1 }
     association :post
+  end
+  factory :user_admin, :class => 'user' do
+    name { 'admin' }
+    email {'admin@test.com'}
+    password {'aaaaaa'}
+    password_confirmation {'aaaaaa'}
+    admin {'true'}
+    position { 'driver' }
+    unit_id { 1 }
+  end
+  factory :user_general, :class => 'user' do
+    name { 'general' }
+    email {'general@test.com'}
+    password {'aaaaaa'}
+    password_confirmation {'aaaaaa'}
+    admin {'false'}
+    position { 'driver' }
+    unit_id { 1 }
   end
 end
